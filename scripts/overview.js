884d96b091cc1987f462aa736b6f9dae5250d490
Joachim <45370332+nooberfrombaldursgate@users.noreply.github.com>
2021-05-08 20:55:08 +0200

    Wipe

--- scripts/overview.js
+++
@@ -1,4 +0,0 @@
-// if (!sessionStorage.getItem('isNewSession')) {
-//   document.querySelector('object').classList.add('slide-in-bottom');
-//   sessionStorage.setItem('isNewSession', true);
-// }
--- scripts/photo-album.js
+++
@@ -1,7 +0,0 @@
-const BACK_ARROW_FALLBACK = document.getElementsByClassName('back-arrow')[1];
-
-BACK_ARROW_FALLBACK.addEventListener('click', goBack);
-
-function goBack() {
-  window.location.href = 'overview.html';
-}
