587ec0601404885a44aa2b7630d8a43cfa1b5709
Joachim <45370332+nooberfrombaldursgate@users.noreply.github.com>
2021-05-18 11:20:21 +0200

    hero text dynamic shift test + image ressources

---
+++ scripts/index.js
@@ -0,0 +1,62 @@
+// get the element
+const text = document.querySelector('.typing-text');
+
+// make a words array
+const words = ['Find spor', 'Løs gåder', 'Knæk koder', 'Opklar mysterier', 'CITY ESCAPE'];
+
+// start typing effect
+setTyper(text, words);
+
+function setTyper(element, words) {
+  let letterTypeDelay = 35;
+  let wordStayDelay = 1000;
+
+  var wordIndex = 0;
+  var letterIndex = 0;
+
+  var wordTypeInterval;
+
+  startTyping();
+
+  function startTyping() {
+    wordTypeInterval = setInterval(typeLetter, letterTypeDelay);
+  }
+
+  function typeLetter() {
+    if (letterIndex == 0 && wordIndex == 0) {
+      text.style.color = 'white';
+      text.style.fontFamily = 'Arial';
+      letterTypeDelay = 35;
+      wordStayDelay = 1000;
+    }
+
+    if (letterIndex == 0 && wordIndex == words.length - 1) {
+      text.style.color = 'var(--gold)';
+      text.style.fontFamily = 'font';
+      text.textTransform = 'capitalize';
+      wordStayDelay = 2000;
+    }
+
+    const word = words[wordIndex];
+    letterIndex++;
+    if (letterIndex == word.length) {
+      clearInterval(wordTypeInterval);
+      setTimeout(startTyping, wordStayDelay);
+
+      setTimeout(nextWord);
+    }
+    if (letterIndex == 0) {
+      nextWord();
+    }
+    const textToType = word.substring(0, letterIndex);
+    element.textContent = textToType;
+  }
+
+  function nextWord() {
+    letterIndex = 0;
+    wordIndex++;
+    if (wordIndex == words.length) {
+      wordIndex = 0;
+    }
+  }
+}
